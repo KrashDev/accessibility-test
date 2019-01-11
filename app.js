22e9e2a9915fb49fabfa418928a1571e4039a9e9
donny <donny.gabai@gmail.com>
2019-01-10 20:45:45 -0500

    CC footer

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
 
 
-});
+  var pos = 0,
+    test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
+  var questions = [
+    ["What is 10 + 4?", "12", "14", "16", "18", "B"],
+    ["What is 12 + 4?", "12", "14", "16", "18", "C"],
+    ["What is 14 + 4?", "12", "14", "16", "18", "D"],
+    ["What is 10 + 2?", "12", "14", "16", "18", "A"]
+  ];
+
+  function renderQuestion() {
+    test = $('#test')
+    testStatus = $('#test_status')
+    testStatus.html = "Question " + (pos + 1) + " of " + questions.length;
+
+  }
+
+
+}); //end of ready function
